076aa36d5d578caf0a6f9e8c901dcea9243a3685
aimeelittle1708-hub <aimeelittle1708@gmail.com>
2026-02-04 14:17:07 +0000

    readies for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,6 +26,7 @@ if os.path.isfile('env.py'):
 
 SECRET_KEY = os.environ.get('SECRET_KEY') 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
