dd3812b64fdef2572ef418ac0d6e081244154f86
aimeelittle1708-hub <aimeelittle1708@gmail.com>
2026-02-10 10:29:02 +0000

    pass argument to a standard function-based view

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+from .models import Event
 
-# Create your views here.
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
-    template_name = "blog/index.html"
-    paginate_by = 6
 
+class EventsList(generic.ListView):
+
+
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+
+
+
+def event_detail(request, event_id):
     
-def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
-
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
 
     return render(
         request,
-        "blog/post_detail.html",
-        {"post": post},
+        "events/event_detail.html",
+        {"event": event}
     )
