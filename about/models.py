ace8af2fa11f6e03d1a0163a3875cd15a95c7bbb
aimeelittle1708-hub <aimeelittle1708@gmail.com>
2026-02-11 15:48:18 +0000

    Fix issues with code, finalise CRUD

--- about/models.py
+++ about/models.py
@@ -1,40 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import User # Import models to connect
-
-STATUS = ((0, "Draft"), (1, "Published"))
+#from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class Post(models.Model):
+
+
+class About(models.Model):
+    """
+    Stores the About Me page content. :model:`about.About`
+    """
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
+#    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["-created_on"]
-    
-    def __str__(self):
-        return f"{self.title} | written by {self.author}"
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
-    body = models.TextField()
-    approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ["created_on"]
+    content = models.TextField()
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
-        
+        return self.title
+
+
+class CollaborateRequest(models.Model):
+    """
+    Stores collaboration requests from users. :model:`about.CollaborateRequest`
+    """
+    name = models.CharField(max_length=200, unique=True)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
